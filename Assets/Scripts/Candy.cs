55b0f12236eca8b9b66ca1700dea228e7c07dcfc
ViliusKk <vilius.kuokstas@gmail.com>
2025-04-28 19:19:03 +0300

    Next level screen

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -1,15 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Candy : MonoBehaviour
 {
+    public GameObject candyVFX;
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Star"))
         {
             Destroy(collision.gameObject);
             UIManager.instance.AddStar();
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spikes"))
+        {
+            Instantiate(candyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
         }
     }
 }
