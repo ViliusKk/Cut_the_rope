3299cf71c8a41aaa0de566414c4dcb90e3bebcd8
ViliusKk <vilius.kuokstas@gmail.com>
2025-05-05 19:15:43 +0300

    Bubble

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    private Rigidbody2D candy;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Candy"))
+        {
+            transform.position = other.transform.position;
+            transform.parent = other.transform;
+            
+            candy = other.gameObject.GetComponent<Rigidbody2D>();
+            candy.gravityScale = -1;
+        }
+        else if (other.gameObject.CompareTag("Cursor") && candy != null)
+        {
+            candy.gravityScale = 1;
+            Destroy(other.gameObject);
+        }
+    }
+}
